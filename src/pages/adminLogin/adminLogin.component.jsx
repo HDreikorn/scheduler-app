f95cf40dff8772c5ecffe7da453c7d4b39b23053
HDreikorn <hillary@dreikorn.com>
2019-11-24 20:48:41 -0600

    fixed login to show loading/ fixed bug in modifying reg dates

--- src/pages/adminLogin/adminLogin.component.jsx
+++ src/pages/adminLogin/adminLogin.component.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Form, Card } from "react-bootstrap";
+import { Button, Form, Card, Spinner } from "react-bootstrap";
 import axios from 'axios';
 import './adminLogin.styles.scss';
 
@@ -8,8 +8,13 @@ class AdminLogin extends Component {
     super(props)
     this.state = {
       username: '',
-      adminPassword: ''
+      adminPassword: '',
+      submitedLogin: false
     }
+  }
+
+  componentDidMount() {
+    fetch('https://highschoolschedulingsystemapi20191019043201.azurewebsites.net/api/healthcheck');
   }
 
   changeHandler = (e) => {
@@ -18,6 +23,7 @@ class AdminLogin extends Component {
 
   submitHandler = (e) => {
     e.preventDefault();
+    this.setState({submitedLogin: true});
     axios.post('https://highschoolschedulingsystemapi20191019043201.azurewebsites.net/api/schoolAdmin/login', this.state)
     .then(response => {
         var location = '/schoolAdmin/'+ response.data.userId;
@@ -31,8 +37,53 @@ class AdminLogin extends Component {
   }
 
   render() {
-    const { username, adminPassword } = this.state;
-    return (
+    const { username, adminPassword, submitedLogin } = this.state;
+    if(submitedLogin){
+      return (
+        <div className="adminLogin">
+            <h1>Log in to your administrator account</h1>
+            <div className="loginCard">
+                <Card body style={{ width: '18rem' }}>
+                <Form onSubmit={this.submitHandler}>
+                    <Form.Group>
+                        <Form.Label>Administrator Username</Form.Label>
+                        <Form.Control 
+                        type="text"
+                        name = "username"
+                        placeholder="Enter username" 
+                        value={ username } 
+                        onChange={this.changeHandler}
+                        />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control 
+                        type="password" 
+                        name="adminPassword"
+                        placeholder="Password" 
+                        value={ adminPassword }
+                        onChange={this.changeHandler}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" disabled>
+                      <Spinner
+                        as="span"
+                        animation="grow"
+                        size="sm"
+                        role="status"
+                        aria-hidden="true"
+                      />
+                      Loading...
+                  </Button>
+                </Form>
+                </Card> 
+            </div>
+        </div>
+    )
+    }
+    else {
+      return (
         <div className="adminLogin">
             <h1>Log in to your administrator account</h1>
             <div className="loginCard">
@@ -67,6 +118,7 @@ class AdminLogin extends Component {
             </div>
         </div>
     )
+    }
   }
 }
 
